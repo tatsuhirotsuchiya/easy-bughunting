16e2b01dc8a2d10d163caf115758f6cec6d965a9
Tatsuhiro Tsuchiya <44872783+tatsuhirotsuchiya@users.noreply.github.com>
2020-11-17 16:07:07 +0900

    Add file via upload

--- onethird.c
+++ onethird.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define NUM_PROC 4
 #define MAX_ROUND 10
@@ -11,49 +12,51 @@
 int decision[NUM_PROC];
 
 int main(int argc, char **argv) {
-    int round = 1;
     //short state[NUM_PROC];
     int state[NUM_PROC];
-    // propose
-    int proc = 0; 
 
+    // Set random seed
     if (argc > 1) {
         srand(atoi(argv[1]));
     }
-    for (proc = 0; proc < NUM_PROC; proc++) {
+    else {
+        srand((unsigned) time(NULL));
+    }
+
+    // Set proposed values randomly 
+    for (int proc = 0; proc < NUM_PROC; proc++) {
         state[proc] = rand() % NUM_PROC + 1;
     }
 
-    for (round = 1; round <= MAX_ROUND; round++) {
+    // Iterate rounds MAX_ROUND times
+    for (int round = 1; round <= MAX_ROUND; round++) {
         int chnl[NUM_PROC][NUM_PROC]; 
         // send
-        for (proc = 0; proc < NUM_PROC; proc++) {
-            int peer = 0;
-            for (peer = 0; peer < NUM_PROC; peer++) {
+        for (int proc = 0; proc < NUM_PROC; proc++) {
+            for (int peer = 0; peer < NUM_PROC; peer++) {
                 chnl[peer][proc] = state[proc];
             } 
         }
         // HO
-        for (proc = 0; proc < NUM_PROC; proc++) {
+        for (int proc = 0; proc < NUM_PROC; proc++) {
             int *ch = chnl[proc];
-            int i = 0;
-            for (i = 0; i < NUM_PROC; i++) {
+            for (int i = 0; i < NUM_PROC; i++) {
                 if (rand() % 2 == 0) {
                     ch[i] = -1; 
                 }
             }
             // print out
-            for (i = 0; i < NUM_PROC; i++) {
+            for (int i = 0; i < NUM_PROC; i++) {
                 printf("%d..", ch[i]);
             }
             printf("\n");
         }
         // receive
-        for (proc = 0; proc < NUM_PROC; proc++) {
+        for (int proc = 0; proc < NUM_PROC; proc++) {
             int *ch = chnl[proc];
             // check if more than 2n/3 messages are received
-            int i = 0, count = 0;
-            for (i = 0; i < NUM_PROC; i++) {
+            int count = 0;
+            for (int i = 0; i < NUM_PROC; i++) {
                 if (ch[i] >= 0) {
                     count++;
                 }
@@ -61,13 +64,13 @@ int main(int argc, char **argv) {
             if (count > TWOTHIRD) {
                 // select smallest most often occurring value
                 int max_count = 0, cand_val = 0;
-                for (i = 0; i < NUM_PROC; i++) {
+                for (int i = 0; i < NUM_PROC; i++) {
                     int val = ch[i];
-                    int j, count = 1;
+                    int count = 1;
                     if (val == -1) {
                         continue;
                     }
-                    for (j = i + 1; j < NUM_PROC; j++) {
+                    for (int j = i + 1; j < NUM_PROC; j++) {
                         if (ch[j] == val)
                             count++;
                     }
@@ -85,9 +88,8 @@ int main(int argc, char **argv) {
         }
         // safety 
         {
-            int i = 0;
             int val = 0;
-            for (i = 0; i < NUM_PROC; i++) {
+            for (int i = 0; i < NUM_PROC; i++) {
                 if (decision[i] == 0) {
                     continue;
                 }
