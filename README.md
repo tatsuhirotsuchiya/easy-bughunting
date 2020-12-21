8fa4155a817e8b89f833c7f8f8b31639b8f05dc5
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2020-12-21 16:28:28 +0900

    add cbmc versions

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# easy-bughunting
+# easy bug hunting
 
 I'm thinking about an easy-to-use approach to debugging of (fault-tolerant) distributed algorithms.
-In the approach, the user (the person who debugs an algorithm) writes a sequential C program that simulates the behavior of the algorithm under debugging. Ordinary testing/debugging tools for C progrmas can be used to write a correct program that faithfully represents the algorithm. Once the user obtains the confidence that the program is correct (in the sense that it correctly represents the algorithm), symbolic testing techniques, such as bounded model checking or symbolic execution, are used to find bugs in the algorithm. 
+In the approach, the user (the person who debugs an algorithm) writes a sequential C program that simulates the behavior of the algorithm under debugging. Ordinary testing/debugging tools for C programs can be used to write a correct program that faithfully represents the algorithm. Once the user obtains the confidence that the program is correct (in the sense that it correctly represents the algorithm), symbolic testing techniques, such as bounded model checking or symbolic execution, are used to find bugs in the algorithm.
+
+## How to apply CBMC on Windows
+
+- Download CBMC binary from [CBMC homepage](https://www.cprover.org/cbmc/)
+- Use Developer Command Prompt for VS to run the CBMC
+ + ``cbmc -DCBMC -DNUM_PROC=5 -DMAX_ROUND=5 filename.c``
---
+++ onethird-v1.c
@@ -0,0 +1,152 @@
+// OneThirdRule
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Number of processes
+#if !defined(NUM_PROC)
+#define NUM_PROC 3
+#endif
+// Number of rounds
+#if !defined(MAX_ROUND)
+#define MAX_ROUND 10
+#endif
+
+#define TWOTHIRD ((int)(2 * NUM_PROC / 3))
+
+int _randint(int first, int last)
+{
+    int tmp;
+#if defined(CBMC)
+    __CPROVER_assume((first <= tmp) && (tmp <= last));
+#else
+    tmp = rand() % (last + 1 - first) + first;
+#endif
+    return tmp;
+}
+
+int decision[NUM_PROC];
+
+int main(int argc, char **argv)
+{
+    //short state[NUM_PROC];
+    int state[NUM_PROC];
+
+#if defined(CBMC)
+    // do nothing
+#else
+    // Set random seed
+    if (argc > 1)
+    {
+        srand(atoi(argv[1]));
+    }
+    else
+    {
+        srand((unsigned)time(NULL));
+    }
+#endif
+
+    // Set proposed values randomly
+    for (int proc = 0; proc < NUM_PROC; proc++)
+    {
+        state[proc] = _randint(1, NUM_PROC);
+    }
+
+    // Iterate rounds MAX_ROUND times
+    for (int round = 1; round <= MAX_ROUND; round++)
+    {
+        // channels
+        int chnl[NUM_PROC][NUM_PROC];
+        // send
+        for (int proc = 0; proc < NUM_PROC; proc++)
+        {
+            for (int peer = 0; peer < NUM_PROC; peer++)
+            {
+                chnl[peer][proc] = state[proc];
+            }
+        }
+        // HO
+        for (int proc = 0; proc < NUM_PROC; proc++)
+        {
+            int *ch = chnl[proc];
+            for (int i = 0; i < NUM_PROC; i++)
+            {
+                if (_randint(0, 1) == 0)
+                {
+                    ch[i] = -1;
+                }
+            }
+            // print out
+            for (int i = 0; i < NUM_PROC; i++)
+            {
+                printf("%d..", ch[i]);
+            }
+            printf("\n");
+        }
+        // receive
+        for (int proc = 0; proc < NUM_PROC; proc++)
+        {
+            int *ch = chnl[proc];
+            // check if more than 2n/3 messages are received
+            int count = 0;
+            for (int i = 0; i < NUM_PROC; i++)
+            {
+                if (ch[i] >= 0)
+                {
+                    count++;
+                }
+            }
+            if (count > TWOTHIRD)
+            {
+                // select smallest most often occurring value
+                int max_count = 0, cand_val = 0;
+                for (int i = 0; i < NUM_PROC; i++)
+                {
+                    int val = ch[i];
+                    int count = 1;
+                    if (val == -1)
+                    {
+                        continue;
+                    }
+                    for (int j = i + 1; j < NUM_PROC; j++)
+                    {
+                        if (ch[j] == val)
+                            count++;
+                    }
+                    if (max_count < count || (max_count == count && val < cand_val))
+                    {
+                        cand_val = val;
+                        max_count = count;
+                    }
+                }
+                printf("proc: %d chooses %d\n.", proc, cand_val);
+                state[proc] = cand_val;
+                if (max_count > TWOTHIRD)
+                {
+                    decision[proc] = cand_val;
+                    printf("proc: %d decides %d\n.", proc, decision[proc]);
+                }
+            }
+        }
+        // safety
+        {
+            int val = 0;
+            for (int i = 0; i < NUM_PROC; i++)
+            {
+                if (decision[i] == 0)
+                {
+                    continue;
+                }
+                else if (val == 0)
+                {
+                    val = decision[i];
+                }
+                else
+                {
+                    assert(val == decision[i]);
+                }
+            }
+        }
+    }
+}
